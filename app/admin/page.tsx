cc019b48b77678ee9515f2c79df4fda56c5bfdce
radu tudor <106737808+imparadu@users.noreply.github.com>
2024-09-05 14:02:37 +0300

    log in working

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,24 +1,64 @@
-export default function AuthPage() {
+// components/Auth.tsx
+"use client"; // Add this lin
+import React, { useState } from "react";
+import { auth } from "../lib/firebaseConfig";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+const Auth: React.FC = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+
+  const handleSignIn = async () => {
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      console.log("User signed in");
+    } catch (error) {
+      console.error("Error signing in:", error);
+    }
+  };
+
   return (
-    <div className="flex justify-center min-h-screen">
-      <form className="flex flex-col space-y-4 p-6 bg-white shadow-md rounded-md">
-        <input
-          type="email"
-          className="px-4 py-2 border rounded-md"
-          placeholder="Email"
-        />
-        <input
-          type="password"
-          className="px-4 py-2 border rounded-md"
-          placeholder="Password"
-        />
-        <button
-          type="submit"
-          className="px-4 py-2 bg-blue-500 text-white rounded-md"
-        >
-          Login
-        </button>
-      </form>
+    <div>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        placeholder="Email"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button onClick={handleSignIn}>Sign In</button>
     </div>
   );
-}
+};
+
+export default Auth;
+
+// export default function AuthPage() {
+//   return (
+//     <div className="flex justify-center min-h-screen">
+//       <form className="flex flex-col space-y-4 p-6 bg-white shadow-md rounded-md">
+//         <input
+//           type="email"
+//           className="px-4 py-2 border rounded-md"
+//           placeholder="Email"
+//         />
+//         <input
+//           type="password"
+//           className="px-4 py-2 border rounded-md"
+//           placeholder="Password"
+//         />
+//         <button
+//           type="submit"
+//           className="px-4 py-2 bg-blue-500 text-white rounded-md"
+//         >
+//           Login
+//         </button>
+//       </form>
+//     </div>
+//   );
+// }
---
+++ app/lib/firebaseConfig.ts
@@ -0,0 +1,22 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC781jIZHLETZHzBDgF9fVfwS2uKcc5Wbo",
+  authDomain: "ruxandra-db-ts.firebaseapp.com",
+  projectId: "ruxandra-db-ts",
+  storageBucket: "ruxandra-db-ts.appspot.com",
+  messagingSenderId: "383452694925",
+  appId: "1:383452694925:web:35ade5a554667d307495c4",
+  measurementId: "G-52EZL6LMVB",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Firebase Auth
+export const auth = getAuth(app); // Ensure this line is present
+export default app; // This line is also necessary
