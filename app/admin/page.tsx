123156b65799dac4c87279168da6ec0a4fdd0d87
radu tudor <106737808+imparadu@users.noreply.github.com>
2024-09-15 00:09:03 +0300

    working db on vercel

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,9 +1,10 @@
 // components/Auth.tsx
 "use client"; // Add this line
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 import { auth } from "../lib/firebaseConfig";
 import { signInWithEmailAndPassword } from "firebase/auth";
+import createTable from "../lib/postres";
 
 const Auth: React.FC = () => {
   const [email, setEmail] = useState<string>("");
@@ -42,28 +43,3 @@ const Auth: React.FC = () => {
 };
 
 export default Auth;
-
-// export default function AuthPage() {
-//   return (
-//     <div className="flex justify-center min-h-screen">
-//       <form className="flex flex-col space-y-4 p-6 bg-white shadow-md rounded-md">
-//         <input
-//           type="email"
-//           className="px-4 py-2 border rounded-md"
-//           placeholder="Email"
-//         />
-//         <input
-//           type="password"
-//           className="px-4 py-2 border rounded-md"
-//           placeholder="Password"
-//         />
-//         <button
-//           type="submit"
-//           className="px-4 py-2 bg-blue-500 text-white rounded-md"
-//         >
-//           Login
-//         </button>
-//       </form>
-//     </div>
-//   );
-// }
