96c449aeee57c3eb340ba4fb5c5c38a38e7df1cd
radu tudor <106737808+imparadu@users.noreply.github.com>
2024-09-27 12:38:34 +0300

    ?? admin interface work

--- app/admin/dashboard/portfolio/page.tsx
+++ app/admin/dashboard/portfolio/page.tsx
@@ -1,15 +1,19 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import { data, ProductCardProps } from "@/app/api/add-pet/data_types";
+import { ProductCard } from "@/app/portfolio/page";
 
 export default function Page() {
-  // const router = useRouter();
-
-  // useEffect(() => {
-  //   const isAuthenticated = false; // Replace with your auth logic
-  //   if (!isAuthenticated) {
-  //     router.push('/admin/login'); // Redirect to login if not authenticated
-  //   }
-  // }, []);
-
-  return <div>Dashboard Content</div>;
-};
+  return (
+    <>
+      <div className="justify-center flex flex-col max-w-lg">
+        <div className="xxs:columns-1 xxs:mx-0 xs:columns-1 xs:mx-0 s:columns-2 md:columns-3 lg:columns-4 gap-0 mx-0">
+          {data.map((product) => {
+            return <ProductCard key={product.id} product={product} />;
+          })}
+        </div>
+        {/* <BackToTopButton /> */}
+      </div>
+    </>
+  );
+}
--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,110 +1,9 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
-import { Picture, Product } from "../api/add-pet/data_types";
+import { Picture, Product, data, ProductCardProps } from "../api/add-pet/data_types";
 
-interface ProductCardProps {
-  product: Product;
-}
 
-const data: Picture[] = [
-  {
-    id: "1", // Changed from number to string
-    title: "pic title",
-    price: 20,
-    currency: "eur",
-    imgUrl: "https://picsum.photos/360/300", // Ensure this URL is less than 255 characters
-    width: 360,
-    height: 300,
-    alt: "random words",
-    created_at: new Date(), // Added created_at
-    updated_at: new Date(), // Added updated_at
-  },
-  {
-    id: "2",
-    title: "pic title",
-    price: 20,
-    currency: "eur",
-    imgUrl: "https://picsum.photos/500/340",
-    width: 500,
-    height: 340,
-    alt: "random wording",
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-  {
-    id: "3",
-    title: "pic title",
-    price: 20,
-    currency: "eur",
-    imgUrl: "https://picsum.photos/400/400",
-    width: 400,
-    height: 400,
-    alt: "random wording",
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-  {
-    id: "4",
-    title: "pic title",
-    price: 20,
-    currency: "eur",
-    imgUrl: "https://picsum.photos/500/350",
-    width: 500,
-    height: 350,
-    alt: "random wording",
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-  {
-    id: "5",
-    title: "pic title",
-    price: 20,
-    currency: "eur",
-    imgUrl: "https://picsum.photos/540/400",
-    width: 540,
-    height: 400,
-    alt: "random wording",
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-  {
-    id: "6",
-    title: "pic title",
-    price: 20,
-    currency: "eur",
-    imgUrl: "https://picsum.photos/400/390",
-    width: 400,
-    height: 390,
-    alt: "random wording",
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-  {
-    id: "7",
-    title: "pic title",
-    price: 20,
-    currency: "eur",
-    imgUrl: "https://picsum.photos/500/340",
-    width: 500,
-    height: 340,
-    alt: "random wording",
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-  {
-    id: "8",
-    title: "pic title",
-    price: 20,
-    currency: "eur",
-    imgUrl: "https://picsum.photos/480/390",
-    width: 480,
-    height: 390,
-    alt: "random wording",
-    created_at: new Date(),
-    updated_at: new Date(),
-  },
-];
 
 export default function Page() {
   return (
@@ -121,7 +20,7 @@ export default function Page() {
   );
 }
 
-const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+export const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
   const fallbackImage = "https://via.placeholder.com/320x480"; // Fallback image URL
   const [imgSrc, setImgSrc] = useState(product.imgUrl || fallbackImage);
 
